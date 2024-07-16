b6433aaccba2c28378fb91eb75639417081544dd
Nitirani2003 <120616903+Nitirani2003@users.noreply.github.com>
2024-07-17 01:11:07 +0530

    Update app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -8,7 +8,7 @@ config();
 const app = express();
 
 //middlewares
-app.use(cors({ origin: "https://mern-ai-frontend-ivory.vercel.app",
+app.use(cors({ origin: "https://vercel.com/niti-rani-rais-projects/mern-ai-fstart",
 methods:["POST", "GET"],
 credentials: true }));
 app.use(express.json());
